06cf30ba154b38ac9fef7d8c01344e2145266413
Lukas Jurisica <lukas.jurisica@gmail.com>
2021-12-07 18:02:16 -0500

    Advent of Code day 7 solution

---
+++ 7/part1.py
@@ -0,0 +1,8 @@
+file = open("input.txt")
+
+numbers = [int(num) for num in file.readline().split(",")]
+median = sorted(numbers)[(len(numbers) + 1) // 2]
+fuelCost = sum([abs(n - median) for n in numbers])
+	
+print("It would take", fuelCost, "units of fuel to align the position")
+file.close
---
+++ 7/part2.py
@@ -0,0 +1,11 @@
+file = open("input.txt")
+
+def fuel(x):
+	return x * (x + 1) // 2
+
+numbers = [int(num) for num in file.readline().split(",")]
+mean = sum(numbers) // len(numbers)
+fuelCost = min([sum([fuel(abs(n - p)) for n in numbers]) for p in [mean, mean + 1]])
+	
+print("It would take", fuelCost, "units of fuel to align the position")
+file.close
